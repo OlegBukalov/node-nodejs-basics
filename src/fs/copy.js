02d7e24b3981770b9ae4a99c5e1ac945d02fc8c5
Oleg Bukalov <60820640+OlegBukalov@users.noreply.github.com>
2024-01-23 23:57:06 +0300

    feature: added copy

--- src/fs/copy.js
+++ src/fs/copy.js
@@ -1,5 +1,33 @@
+import {fileURLToPath} from 'url';
+import path from 'path';
+import fs from 'fs';
+
 const copy = async () => {
-    // Write your code here 
+    const __filename = fileURLToPath(import.meta.url);
+    const __dirname = path.dirname(__filename);
+
+    const sourceFolderPath = path.join(__dirname, 'files');
+    const targetFolderPath = path.join(__dirname, 'files_copy');
+
+    if (!fs.existsSync(sourceFolderPath) || fs.existsSync(targetFolderPath)) {
+        throw new Error('FS operation failed');
+    }
+
+    try {
+        fs.mkdirSync(targetFolderPath);
+
+        const files = fs.readdirSync(sourceFolderPath);
+
+        files.forEach((file) => {
+            const sourcePath = path.join(sourceFolderPath, file);
+            const targetPath = path.join(targetFolderPath, file);
+
+            fs.copyFileSync(sourcePath, targetPath);
+        });
+
+    } catch (error) {
+        console.log(error);
+    }
 };
 
 await copy();
