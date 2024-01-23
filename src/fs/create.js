3498b8ca92a27ac922c6a0b1e29dc973642c44d5
Oleg Bukalov <60820640+OlegBukalov@users.noreply.github.com>
2024-01-23 23:00:14 +0300

    refactor: removed code from main branch

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,26 +1,8 @@
-import {fileURLToPath} from 'url';
 import path from 'path';
-import fs from 'fs/promises';
 
 const create = async () => {
-    const __filename = fileURLToPath(import.meta.url);
-    const __dirname = path.dirname(__filename);
-    const filePath = path.resolve(__dirname, 'files', 'fresh.txt');
-
-    try {
-        await fs.access(filePath);
-        throw new Error('FS operation failed');
-    } catch (error) {
-        if (error.code === 'ENOENT') {
-            try {
-                await fs.writeFile(filePath, 'I am fresh and young');
-            } catch (error) {
-                console.log(error);
-            }
-        } else {
-            console.log(error);
-        }
-    }
+    const filePath = path.join('./files', 'fresh.txt');
+    console.dir(path.resolve(__dirname));
 };
 
 await create();
