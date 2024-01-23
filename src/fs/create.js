fd63578562f917262597bbf54f4b6b120babd4b4
Oleg Bukalov <60820640+OlegBukalov@users.noreply.github.com>
2024-01-23 23:01:58 +0300

    feature: added creating new file

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,8 +1,26 @@
+import {fileURLToPath} from 'url';
 import path from 'path';
+import fs from 'fs/promises';
 
 const create = async () => {
-    const filePath = path.join('./files', 'fresh.txt');
-    console.dir(path.resolve(__dirname));
+    const __filename = fileURLToPath(import.meta.url);
+    const __dirname = path.dirname(__filename);
+    const filePath = path.resolve(__dirname, 'files', 'fresh.txt');
+
+    try {
+        await fs.access(filePath);
+        throw new Error('FS operation failed');
+    } catch (error) {
+        if (error.code === 'ENOENT') {
+            try {
+                await fs.writeFile(filePath, 'I am fresh and young');
+            } catch (error) {
+                console.log(error);
+            }
+        } else {
+            console.log(error);
+        }
+    }
 };
 
 await create();
