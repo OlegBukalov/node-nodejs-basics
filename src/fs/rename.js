c6a3fd508fed377c277a44e713344f498ffbcc7c
Oleg Bukalov <60820640+OlegBukalov@users.noreply.github.com>
2024-01-25 21:48:45 +0300

    feature: added rename

--- src/fs/rename.js
+++ src/fs/rename.js
@@ -1,5 +1,23 @@
+import {fileURLToPath} from 'url';
+import path from 'path';
+import fs from 'fs';
+
 const rename = async () => {
-    // Write your code here 
+    const __filename = fileURLToPath(import.meta.url);
+    const __dirname = path.dirname(__filename);
+
+    const wrongFilePath = path.join(__dirname, 'files_copy', 'wrongFilename.txt');
+    const correctFilePath = path.join(__dirname, 'files_copy', 'properFilename.md');
+
+    if (!fs.existsSync(wrongFilePath) || fs.existsSync(correctFilePath)) {
+        throw new Error('FS operation failed');
+    }
+
+    try {
+        fs.renameSync(wrongFilePath, correctFilePath);
+    } catch (error) {
+        console.log(error);
+    }
 };
 
 await rename();
