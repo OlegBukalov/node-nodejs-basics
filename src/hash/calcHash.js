05cd1cda31dca1052f2682309760d163d4cda445
Oleg Bukalov <60820640+OlegBukalov@users.noreply.github.com>
2024-01-25 22:59:44 +0300

    feature: added calculateHash

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,24 @@
+import {fileURLToPath} from 'url';
+import path from 'path';
+import fs from 'fs';
+import crypto from 'crypto';
+
 const calculateHash = async () => {
-    // Write your code here 
+    const __filename = fileURLToPath(import.meta.url);
+    const __dirname = path.dirname(__filename);
+
+    const filePath = path.join(__dirname, 'files', 'fileToCalculateHashFor.txt');
+    const fileStream = fs.createReadStream(filePath);
+    const hash = crypto.createHash('sha256');
+
+    fileStream.on('data', data => {
+        hash.update(data);
+    });
+
+    fileStream.on('end', () => {
+        const fileHash = hash.digest('hex');
+        console.log(fileHash);
+    });
 };
 
 await calculateHash();
