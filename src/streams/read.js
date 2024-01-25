c42a1e2cd7a14469482cf3a194f5645270770464
Oleg Bukalov <60820640+OlegBukalov@users.noreply.github.com>
2024-01-26 00:10:04 +0300

    feature: added read

--- src/streams/read.js
+++ src/streams/read.js
@@ -1,5 +1,21 @@
+import {fileURLToPath} from 'url';
+import path from 'path';
+import fs from 'fs';
+
 const read = async () => {
-    // Write your code here 
+    const __filename = fileURLToPath(import.meta.url);
+    const __dirname = path.dirname(__filename);
+
+    const filePath = path.join(__dirname, 'files', 'fileToRead.txt');
+    const fileStream = fs.createReadStream(filePath);
+
+    fileStream.on('data', chunk => {
+        process.stdout.write(chunk);
+    });
+
+    fileStream.on('end', () => {
+        process.stdout.write('\n');
+    });
 };
 
 await read();
