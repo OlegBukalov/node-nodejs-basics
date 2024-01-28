144c92bab831d82171844e02ab4703742fa45ef5
Oleg Bukalov <60820640+OlegBukalov@users.noreply.github.com>
2024-01-28 22:42:34 +0300

    feature: added write

--- src/streams/write.js
+++ src/streams/write.js
@@ -1,5 +1,15 @@
+import {fileURLToPath} from 'url';
+import path from 'path';
+import fs from 'fs';
+
 const write = async () => {
-    // Write your code here 
+    const __filename = fileURLToPath(import.meta.url);
+    const __dirname = path.dirname(__filename);
+
+    const filePath = path.join(__dirname, 'files', 'fileToWrite.txt');
+    const fileStream = fs.createWriteStream(filePath);
+
+    process.stdin.pipe(fileStream);
 };
 
 await write();
